d5b5911a1160548d64c1c527431e801fc4a554c3
Matt Lea <mlea@schematical.com>
2014-10-16 15:42:20 -0500

    Code flying from the fingertips

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -2,31 +2,30 @@ module.exports = function(grunt) {
 	var config = require('./config');
 	var repos = [];
 	var forever =  {};
+	var grunt_config = {
+		config: {
+
+		},
+		gitPull: {},
+		forever: {}
+	}
 	for(var i in config.apps){
-		repos.push({
-			path:  config.apps[i].path,
-			repo: config.apps[i].repo
-		});
-		forever[i] =  {
+		grunt_config.gitPull[i] = {
+			repos:[{
+				path:  config.apps[i].path,
+				repo: config.apps[i].repo
+			}]
+		};
+
+		grunt_config.forever[i] =  {
 			options: {
 				index: config.apps[i].path + '/app.js',
 				logDir: 'logs'
 			}
 		}
 	}
-	grunt.initConfig({
-		config: {
 
-		},
-		gitPull: {
-			example: {
-				repos: [
-
-				]
-			}
-		},
-		forever: forever
-	});
+	grunt.initConfig(grunt_config);
 
 	grunt.registerTask('deploy', function(app_id){
 		grunt.task.run('gitPull:' + app_id, 'forever:' + app_id + ':restart');
